fedb9562e59e6df1468fc41143a4360e2e359cf6
Brojowski <alex.gajowski@gmail.com>
2017-04-16 15:52:45 -0400

    add user to core

---
+++ User.java
@@ -0,0 +1,40 @@
+package com.example.b2c_core;
+
+/**
+ *
+ */
+public class User
+{
+    private static int i = 0;
+    private String uname;
+
+    private User(String username)
+    {
+        uname = username;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o instanceof String)
+        {
+            String toCompare = (String)o;
+            return uname.compareTo(toCompare) == 0;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return uname.hashCode();
+    }
+
+    public static User exampleUser()
+    {
+        return new User("Test"+ i++);
+    }
+}

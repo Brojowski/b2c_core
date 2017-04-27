224a0a08d6963aa4a83cfd9ab1291fbb98b455fe
Brojowski <alex.gajowski@gmail.com>
2017-04-27 16:14:22 -0400

    PlaceTileTransferObject created

---
+++ PlaceTileTransferObject.java
@@ -0,0 +1,13 @@
+package com.example.b2c_core;
+
+/**
+ * Created by alex on 4/27/17.
+ */
+public class PlaceTileTransferObject
+{
+    public User currentUser;
+    public BuildingType tileToPlace;
+    public SharedCity targetCity;
+    public int x;
+    public int y;
+}
